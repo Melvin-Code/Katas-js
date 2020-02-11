16aa569623d0cb04ebbb9dd7bcaadb68177d9fc6
melvin-Code <melvingonzalez470@gmail.com>
2020-02-11 09:33:15 -0500

    cdrolling text kata added

---
+++ scrollingText.js
@@ -0,0 +1,9 @@
+function scrollingText(text){
+    let result = []
+     
+     for( var i = 0; i < text.length; i++){
+       result.push((text.slice(i) + text.slice(0,i)).toUpperCase())
+     }
+     
+     return result
+   }
