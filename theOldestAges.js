f0a9cb310cd05d718675dd513b42f9768a664143
melvin-Code <melvingonzalez470@gmail.com>
2020-02-24 09:50:40 -0500

    twoOldestAges kata

---
+++ theOldestAges.js
@@ -0,0 +1,21 @@
+function twoOldestAges(ages){
+let result = []
+let copyofage = ages.sort((a, b)=>{
+  return b - a
+})
+copyofage.splice(2)
+copyofage.sort((a, b)=>{
+  return a - b
+})
+return copyofage
+}
+
+twoOldestAges(ages)
+
+//short answer
+
+function twoOldestAges(ages){
+return ages.sort((a,b)=>{return a-b}).slice(-2)
+}
+
+twoOldestAges(ages)
